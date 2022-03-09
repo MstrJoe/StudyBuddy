c523642b6a3cf8a78c113f9c6912eb8660151747
MstrJoe <joeysbakker@gmail.com>
2022-03-09 15:22:57 +0100

    mappings complete

--- backend/src/main/java/com/studybuddy/api/controller/SubjectController.java
+++ backend/src/main/java/com/studybuddy/api/controller/SubjectController.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/subject")
@@ -31,4 +33,30 @@ public class SubjectController {
         return new ResponseEntity<>(subject, HttpStatus.CREATED);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Subject> createSubject(@PathVariable Long id, @RequestBody SubjectDto data) {
+        Optional<Subject> currentSubject = this.subjectRepository.findById(id);
+
+        if (currentSubject.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Subject not found");
+        }
+
+        Subject subject = currentSubject.get();
+        subject.setName(data.getName());
+        this.subjectRepository.save(subject);
+        return new ResponseEntity<>(subject, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Subject> deleteSubject(@PathVariable Long id) {
+        Optional<Subject> currentSubject = this.subjectRepository.findById(id);
+
+        if (currentSubject.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Subject not found");
+        }
+
+        Subject subject = currentSubject.get();
+        this.subjectRepository.delete(subject);
+        return new ResponseEntity<>(subject, HttpStatus.OK);
+    }
 }
