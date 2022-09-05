9d05dc1deafba564585f128a6bb327d442536cdd
MstrJoe <joeysbakker@gmail.com>
2022-09-05 08:47:23 +0200

    How it works page done

--- frontend/src/pages/how-it-works.jsx
+++ frontend/src/pages/how-it-works.jsx
@@ -4,15 +4,46 @@ export function HowItWorksPage() {
       <div className="page-header">
         <h1>How it works</h1>
       </div>
-      <h2>It's really easy:</h2>
 
-      <h2>As a teacher</h2>
+      <p>
+        This app is created so that students of any online schooling program can find moments where
+        they can do homework together.
+        <br />
+        This might help make it easier to form groups in the class which makes every study more fun
+        and easy!
+      </p>
+      <h4>So, what can you do?</h4>
 
-      <p>Dit kan je als teacher</p>
+      <h2>Logged in as a student you can:</h2>
 
-      <h2>As a student</h2>
+      <p>
+        - Add an agenda item
+        <br />
+        This item can be about homework that a teacher has put in the system,
+        <br />
+        or it can be about any subject you would like to work on together with other students.
+        <br />- You can subscribe to somebody else's agenda item if you would like to join that
+        homework moment.
+        <br />
+        <br />
+        <p>You have subscribers on your item or you subscribed on somebody's item, then what? </p>
+        Find and join eachother on teams or the system that is being used by your school and join
+        and start your study together!
+      </p>
 
-      <p>Dit kan je als student</p>
+      <h2>Logged in as a teacher you can:</h2>
+
+      <p>
+        - Go to the subject page and add a subject (class)
+        <br />- Add homework for the students within a subject
+        <br />- Edit, change or delete the subjects or homework you have created yourself
+        <br />- Go to the agenda page and see what the students have put on it, so you might also
+        see where the students are struggling most
+      </p>
+
+      <br />
+      <br />
+      <h3>StudyBuddy, Stronger Together!</h3>
     </>
   );
 }
